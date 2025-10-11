831734fef66ae775ca18b53deeb6511d997f1024
Bruna <brunameiraf@gmail.com>
2025-10-11 15:55:09 -0300

    Adicionei dados fixture

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,8 +7,10 @@ describe('Login', () => {
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('123456')
+    cy.fixture('credenciais').then(credenciais => {
+      cy.get('#username').click().type(credenciais.valida.usuario)
+      cy.get('#senha').click().type(credenciais.valida.senha)
+    })
     cy.contains('button', 'Entrar').click()
 
     //Assert
@@ -17,8 +19,9 @@ describe('Login', () => {
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     // Act
-    cy.get('#username').click().type('julio.lima')
-    cy.get('#senha').click().type('654321')
+    cy.fixture('credenciais').then(credenciais => {
+     cy.get('#username').click().type(credenciais.invalida.usuario)
+      cy.get('#senha').click().type(credenciais.invalida.senha)})
     cy.contains('button', 'Entrar').click()
 
     //Assert
