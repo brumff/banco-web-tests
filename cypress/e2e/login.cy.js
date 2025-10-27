63f05d0cc44321dacad7f8a4a1370fa175f31bbf
Bruna Meira Fernandes <brunameiraf@gmail.com>
2025-10-27 15:21:46 -0300

    Implementando comandos customizaveis para login e transferencias

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,11 +7,7 @@ describe('Login', () => {
 
   it('Login com dados válidos deve permitir entrada no sistema', () => {
     // Act
-    cy.fixture('credenciais').then(credenciais => {
-      cy.get('#username').click().type(credenciais.valida.usuario)
-      cy.get('#senha').click().type(credenciais.valida.senha)
-    })
-    cy.contains('button', 'Entrar').click()
+    cy.fazerLoginComCredenciaisValidas()
 
     //Assert
     cy.contains('h4', 'Realizar Transferência').should('be.visible')
@@ -19,12 +15,9 @@ describe('Login', () => {
 
   it('Login com dados inválidos deve apresentar mensagem de erro', () => {
     // Act
-    cy.fixture('credenciais').then(credenciais => {
-     cy.get('#username').click().type(credenciais.invalida.usuario)
-      cy.get('#senha').click().type(credenciais.invalida.senha)})
-    cy.contains('button', 'Entrar').click()
+    cy.fazerLoginComCredenciaisInvalidas()
 
     //Assert
-    cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
+    cy.verificarMensagemNoToast('Erro no login. Tente novamente.')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+//Comandos customizados para interagir com a aplicação
+import './commands/common'
+//Comando customizados para interagir com funcionalidades especificas
+import './commands/login'
+import './commands/transferencias'
